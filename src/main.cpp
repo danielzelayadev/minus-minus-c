11061823d2e6fafb86ff39ac3e2187a6301d0138
Daniel Zelaya <wupaz9@gmail.com>
2017-09-03 23:03:49 -0600

    Setup yylex for sample

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
+extern FILE* yyin;
+int yylex();
+
 int main(int argc, char **argv) {
+
+    if (argc != 2) {
+        printf("Usage: %s [FILE]\n", argv[0]);
+        return 1;
+    }
+
+    yyin = fopen(argv[1], "r");
+
+    if (!yyin) {
+        printf("File '%s' not found.\n", argv[1]);
+        return 1;
+    }
+
+    while(yylex());
+
     return 0;
 }
