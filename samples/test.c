3ea1c4f027a863bbd16bd7d312923dbf715baf2e
Daniel Zelaya <wupaz9@gmail.com>
2017-09-05 17:20:23 -0600

    Test quicksort

--- samples/test.c
+++ samples/test.c
@@ -1,10 +1,36 @@
-void func(int argc, char *argv) {
-    int x = 10;
-    int y = 100;
+/* A typical recursive C/C++  implementation of QuickSort */
+ 
+/* This function takes last element as pivot, places 
+the pivot element at its correct position in sorted 
+array, and places all smaller (smaller than pivot)
+to left of pivot and all greater elements to right 
+of pivot */
+int partition (int arr[], int l, int h) {
+    int x = arr[h];
+    int i = (l - 1);
+    int j;
 
-    x = 45;
-    x++;
+    for (j = l; j <= h- 1; j++)
+    {
+        if (arr[j] <= x)
+        {
+            i++;
+            swap (&arr[i], &arr[j]);
+        }
+    }
+    swap (&arr[i + 1], &arr[h]);
+    return (i + 1);
 }
 
-int x, y = sizeof(sizeof 10 + 2 * sizeof(x)), z ,zz;
-int u;
+/* A[] --> Array to be sorted, 
+    l  --> Starting index, 
+    h  --> Ending index */
+void quickSort(int A[], int l, int h) {
+    if (l < h)
+    {        
+        /* Partitioning index */
+        int p = partition(A, l, h); 
+        quickSort(A, l, p - 1);  
+        quickSort(A, p + 1, h);
+    }
+}
