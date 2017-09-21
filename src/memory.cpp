bde14a5b5cc331050e199152ad6546a7f99fa498
Daniel Zelaya <wupaz9@gmail.com>
2017-09-21 02:53:23 -0600

    Remove unnecessary quotations

--- src/memory.cpp
+++ src/memory.cpp
@@ -13,7 +13,7 @@ map<string, string> data;
 string newString(string literal) {
     string label = newLabel("str");
 
-    data[label] = label + ": .asciiz \"" + literal + "\"";
+    data[label] = label + ": .asciiz " + literal;
 
     return label;
 }
