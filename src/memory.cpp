a7c849114e489ee6a9208882c52fe0db2321c31c
Daniel Zelaya <wupaz9@gmail.com>
2017-09-21 22:57:57 -0600

    Add class for stack memory management

--- src/memory.cpp
+++ src/memory.cpp
@@ -1,5 +1,6 @@
 #include "memory.h"
 #include "helpers.h"
+#include "utils.h"
 #include <iostream>
 #include <stdlib.h>
 #include <map>
@@ -10,12 +11,71 @@
 bool temps[TEMPCOUNT];
 map<string, string> data;
 
+void Stack::push(string id, int siz) {
+    _stack->push_back(new StackData(id, siz));
+}
+
+void Stack::pushFrame() {
+    push(to_string(ebpPos), 4);
+    ebpPos = _stack->size() - 1;
+}
+
+void Stack::popFrame() {
+    StackData *sd;
+
+    while (!isNumber((sd = _stack->back())->id)) {
+        delete sd;
+        _stack->pop_back();
+    }
+
+    ebpPos = atoi(sd->id.c_str());
+
+    delete sd;
+    _stack->pop_back();
+}
+
+int Stack::getBaseOffset(string id) {
+    int offset = 4;
+
+    if (ebpPos == -1) return 0;
+
+    for (int i = ebpPos + 1; i < _stack->size(); i++) {
+        StackData *sd = (*_stack)[i];
+        
+        if (sd->id == id)
+            return offset * -1;
+
+        offset += sd->siz;
+    }
+
+    offset = 4;
+
+    for (int i = ebpPos - 1; i >=1; i--) {
+        StackData *sd = (*_stack)[i];
+        
+        if (sd->id == id)
+            return offset;
+            
+        offset += sd->siz;
+    }
+
+    return 0;
+}
+
 string newString(string literal) {
     string label = newLabel("str");
 
     data[label] = label + ": .asciiz " + literal;
 
     return label;
+}
+
+void newInt(string id) {
+    data[id] = id + ": .word 0";
+}
+
+void newChar(string id) {
+    data[id] = id + ": .byte 0";
 }
 
 int getFirstFreeTemp() {
--- src/memory.h
+++ src/memory.h
@@ -2,10 +2,35 @@
 #define  MEMORY
 
 #include <string>
+#include <vector>
 
 using namespace std;
 
+class StackData {
+public:
+    string id;
+    int siz;
+    StackData(string id, int siz) {
+        this->id = id;
+        this->siz = siz;
+    }
+};
+
+class Stack {
+private:
+    vector<StackData*> *_stack;
+    int ebpPos;
+public:
+    Stack() { ebpPos = -1; _stack = new vector<StackData*>(); }
+    void push(string, int);
+    void pushFrame();
+    void popFrame();
+    int getBaseOffset(string);
+};
+
 string newString(string = "");
+void newInt(string = "");
+void newChar(string = "");
 int newTemp();
 void freeTemp(int);
 
