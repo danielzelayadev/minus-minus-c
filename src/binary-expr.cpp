5e13b853299f52144f5ab9f564c96ed8f10ab38f
Daniel Zelaya <wupaz9@gmail.com>
2017-09-22 02:54:42 -0600

    All binary expressions except for conditionals

--- src/binary-expr.cpp
+++ src/binary-expr.cpp
@@ -2,6 +2,32 @@
 #include "memory.h"
 #include "helpers.h"
 
+#define GENERIC_BINARY_CODEGEN(name, instr) \
+string name##Expression::genCode() { \
+    string code; \
+    code += left->genCode(); \
+    code += right->genCode(); \
+    place = left->place; \
+    code += string(instr) + " " + toRegStr(place) + ", " + \
+    toRegStr(left->place) + ", " + toRegStr(right->place) + "\n"; \
+    freeTemp(right->place); \
+    return code; \
+}
+
+#define MULTIPLICATIVE_BINARY_CODEGEN(name, func) \
+string name##Expression::genCode() { \
+    string code; \
+    code += left->genCode(); \
+    code += right->genCode(); \
+    place = left->place; \
+    code += move("$a0", toRegStr(left->place)); \
+    code += move("$a1", toRegStr(right->place)); \
+    code += "jal " + string(func) + "\n"; \
+    code += move(toRegStr(place), "$v0"); \
+    freeTemp(right->place); \
+    return code; \
+}
+
 BinaryExpression::BinaryExpression(Expression *left, Expression *right, string op) {
     this->left = left;
     this->right = right;
@@ -23,3 +49,29 @@ string AssignmentExpression::genCode() {
 
     return code;
 }
+
+string CondAndExpression::genCode() {
+    return "";
+}
+
+string CondOrExpression::genCode() {
+    return "";
+}
+
+GENERIC_BINARY_CODEGEN(Or, "or")
+GENERIC_BINARY_CODEGEN(Xor, "xor")
+GENERIC_BINARY_CODEGEN(And, "and")
+GENERIC_BINARY_CODEGEN(Equals, "seq")
+GENERIC_BINARY_CODEGEN(NotEquals, "sne")
+GENERIC_BINARY_CODEGEN(LessThan, "slt")
+GENERIC_BINARY_CODEGEN(GreaterThan, "sgt")
+GENERIC_BINARY_CODEGEN(Lte, "sle")
+GENERIC_BINARY_CODEGEN(Gte, "sge")
+GENERIC_BINARY_CODEGEN(ShiftLeft, "sll")
+GENERIC_BINARY_CODEGEN(ShiftRight, "slr")
+GENERIC_BINARY_CODEGEN(Sum, "add")
+GENERIC_BINARY_CODEGEN(Sub, "sub")
+
+MULTIPLICATIVE_BINARY_CODEGEN(Mul, "mult")
+MULTIPLICATIVE_BINARY_CODEGEN(Div, "divide")
+MULTIPLICATIVE_BINARY_CODEGEN(Mod, "mod")
