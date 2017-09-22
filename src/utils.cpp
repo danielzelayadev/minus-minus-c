de50a72c289068dd1efd49ce9f3f7a19a4eed78a
Daniel Zelaya <wupaz9@gmail.com>
2017-09-21 22:56:55 -0600

    Add isNumber to utils

--- src/utils.cpp
+++ src/utils.cpp
@@ -6,3 +6,10 @@ uint64_t uniqueId(uint16_t v1, uint16_t v2, uint16_t v3, uint16_t v4)
    id = v1 | (((uint64_t)v2) << 16) | (((uint64_t)v3) << 32) | (((uint64_t)v4) << 48);
    return id;
 }
+
+bool isNumber(string str) {
+    for (int i = 0; i < str.size(); i++)
+        if (!isdigit(str[i]) && (i == 0 && str[i] != '-'))
+            return false;
+    return true;
+}
--- src/utils.h
+++ src/utils.h
@@ -2,7 +2,11 @@
 #define UTILS
 
 #include <stdint.h>
+#include <string>
+
+using namespace std;
 
 uint64_t uniqueId(uint16_t, uint16_t, uint16_t, uint16_t);
+bool isNumber(string);
 
 #endif
