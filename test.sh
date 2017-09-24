ffbe056bc779a555793d559ec52684273c3697d0
Daniel Zelaya <wupaz9@gmail.com>
2017-09-24 00:56:12 -0600

    If no args provided, run the test on all samples

--- test.sh
+++ test.sh
@@ -1,5 +1,31 @@
 if [ "$#" -lt 1 ]; then
-    echo "You must provide an input file!"
+    ./bin/mmc samples/HelloWorld.c vmipssoc/src/main.S
+    cd vmipssoc/src
+    make run
+    cd ../..
+    ./bin/mmc samples/ForTest.c vmipssoc/src/main.S
+    cd vmipssoc/src
+    make run
+    cd ../..
+    ./bin/mmc samples/IfTest.c vmipssoc/src/main.S
+    cd vmipssoc/src
+    make run
+    cd ../..
+    ./bin/mmc samples/WhileTest.c vmipssoc/src/main.S
+    cd vmipssoc/src
+    make run
+    cd ../..
+    ./bin/mmc samples/ArithOperators.c vmipssoc/src/main.S
+    cd vmipssoc/src
+    make run
+    cd ../..
+    ./bin/mmc samples/RelOperators.c vmipssoc/src/main.S
+    cd vmipssoc/src
+    make run
+    cd ../..
+    ./bin/mmc samples/Globals.c vmipssoc/src/main.S
+    cd vmipssoc/src
+    make run
 else
     ./bin/mmc "$1" vmipssoc/src/main.S
     cd vmipssoc/src
