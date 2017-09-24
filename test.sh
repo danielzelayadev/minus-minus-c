3300414309aab5eb26efcd1c972c33cc7f593db4
Daniel Zelaya <wupaz9@gmail.com>
2017-09-24 15:34:16 -0600

    Use for loop to test all samples

--- test.sh
+++ test.sh
@@ -1,35 +1,10 @@
 if [ "$#" -lt 1 ]; then
-    ./bin/mmc samples/HelloWorld.c vmipssoc/src/main.S
-    cd vmipssoc/src
-    make run
-    cd ../..
-    ./bin/mmc samples/ForTest.c vmipssoc/src/main.S
-    cd vmipssoc/src
-    make run
-    cd ../..
-    ./bin/mmc samples/IfTest.c vmipssoc/src/main.S
-    cd vmipssoc/src
-    make run
-    cd ../..
-    ./bin/mmc samples/WhileTest.c vmipssoc/src/main.S
-    cd vmipssoc/src
-    make run
-    cd ../..
-    ./bin/mmc samples/ArithOperators.c vmipssoc/src/main.S
-    cd vmipssoc/src
-    make run
-    cd ../..
-    ./bin/mmc samples/RelOperators.c vmipssoc/src/main.S
-    cd vmipssoc/src
-    make run
-    cd ../..
-    ./bin/mmc samples/Globals.c vmipssoc/src/main.S
-    cd vmipssoc/src
-    make run
-    cd ../..
-    ./bin/mmc samples/MultipleFunctions.c vmipssoc/src/main.S
-    cd vmipssoc/src
-    make run
+    for f in samples/*.c ; do  
+        ./bin/mmc "$f" vmipssoc/src/main.S
+        cd vmipssoc/src
+        make run
+        cd ../.. ; 
+    done
 else
     ./bin/mmc "$1" vmipssoc/src/main.S
     cd vmipssoc/src
