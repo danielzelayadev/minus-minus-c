563a1da8ffb89ebdfa793746c894b2f3b1740aef
Daniel Zelaya <wupaz9@gmail.com>
2017-09-25 03:44:00 -0600

    Add size getter for expressions

--- src/ast/expressions/expression.h
+++ src/ast/expressions/expression.h
@@ -6,6 +6,7 @@
 class Expression : public ASTNode {
 public:
     int place;
+    virtual int size() { return 0; }
     virtual string genCode(bool preserve = false) { return ""; }
     virtual bool isAssignable() { return false; }
     virtual string genAddrCode(bool preserve = false) { return ""; }
