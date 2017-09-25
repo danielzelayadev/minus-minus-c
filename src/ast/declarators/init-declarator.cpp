7e20e39e21208508e18c14feb8b2a7e73de27a08
Daniel Zelaya <wupaz9@gmail.com>
2017-09-25 02:22:05 -0600

    Array declarations and initializations

--- src/ast/declarators/init-declarator.cpp
+++ src/ast/declarators/init-declarator.cpp
@@ -6,6 +6,7 @@
 #include "../../code-gen/memory.h"
 #include "../../code-gen/helpers.h"
 #include <iostream>
+#include <stdlib.h>
 
 extern Stack *callStack;
 extern int currScope;
@@ -24,7 +25,68 @@ string InitDeclarator::genCode(DataType dt) {
     if (dynamic_cast<FunctionDeclarator*>(declarator)) return "";
 
     if (arrDecl) {
-        cout << "TODO: Support array declarations\n";
+         int dims;
+        ArrayInitializer *arrInit = 0;
+        
+        if (arrDecl->dimExpr)
+            dims = atoi(((IntExpression*)arrDecl->dimExpr)->literal->c_str());
+        else {
+            arrInit = ((ArrayInitializer*)initializer);
+            dims = arrInit->exprList->size();
+        }
+
+        if (!currScope)
+            if (dt == INT)
+                newInt(id, dims);
+            else
+                newChar(id, dims);
+        else
+            if (dt == INT)
+                code += allocIntArray(id, dims);
+            else
+                code += allocCharArray(id, dims);
+
+        if (initializer) {
+            arrInit = arrInit ? arrInit : ((ArrayInitializer*)initializer);
+            vector<Expression*> *initExprs = arrInit->exprList;
+            if (!currScope) {
+                int baseAddr = newTemp();
+                string baseStr = toRegStr(baseAddr);
+
+                globalInits += "la " + baseStr + ", " + id + "\n";
+
+                for (int i = 0; i < initExprs->size(); i++) {
+                    int offset = i * (dt == INT ? 4 : 1);
+                    Expression *expr = (*initExprs)[i];
+                    globalInits += expr->genCode();
+
+                    globalInits += sw(toRegStr(expr->place), offset, baseStr);
+
+                    freeTemp(expr->place);
+                }
+
+                freeTemp(baseAddr);
+            }
+            else {
+                int baseAddr;
+                code += useSaved(&baseAddr);
+                string baseStr = toRegStr(baseAddr, 's');
+
+                code += lw(baseStr, callStack->getBaseOffset(id), "$fp");
+
+                for (int i = 0; i < initExprs->size(); i++) {
+                    int offset = i * (dt == INT ? 4 : 1);
+                    Expression *expr = (*initExprs)[i];
+                    code += expr->genCode();
+
+                    code += sw(toRegStr(expr->place), offset, baseStr);
+
+                    freeTemp(expr->place);
+                }
+
+                code += releaseSaved(baseAddr);
+            }
+        }
     } else {
         Expression *initExpr = initializer ? ((VarInitializer*)initializer)->expr : 0;
 
