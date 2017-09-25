3ce8ea753f8ee0f3067b8d9ee20c6287de57be73
Daniel Zelaya <wupaz9@gmail.com>
2017-09-25 15:12:11 -0600

    Add PrintStr sample

---
+++ samples/PrintStr.c
@@ -0,0 +1,29 @@
+void main() {
+    char str[12] = { 'A', 'e', 'l', 'l', 'o', '!', ' ', 'S', 't', 'r', '!', '\0' };
+    int i = 10;
+    char d = str[0];
+    // char c = d;//str[0];
+
+    // for (i = 1; i < 11; i++) {
+        // put_char(c);
+        put_char(d);
+        // c = str[i];
+    // }
+
+    put_char('\n');
+
+    
+    // printStr(myStr);
+}
+
+// void printStr(char str[]) {
+//     int i;
+//     char c = str[0];
+
+//     for (i = 1; i < 11; i++) {
+//         put_char(c);
+//         c = str[i];
+//     }
+
+//     put_char('\n');
+// }
